fff3546e19de1baf3752da8e50557c979106df6c
Timo Weiss <timo.weiss@protonmail.ch>
2024-08-27 14:52:25 +0200

    inital app setup

---
+++ virtualchime/virtualchime/virtualchimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualchimeApp.swift
+//  virtualchime
+//
+//  Created by Timo Weiß on 26.08.24.
+//
+
+import SwiftUI
+
+@main
+struct virtualchimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
